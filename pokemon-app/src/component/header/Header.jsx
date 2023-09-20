426819e73f71b4b3aa6b273a59b6186357b51e07
keyshaar <keysha.aira@gmail.com>
2023-09-20 22:13:14 +0700

    prolem with header

--- pokemon-app/src/component/header/Header.jsx
+++ pokemon-app/src/component/header/Header.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import { useState } from 'react'
+import pokedex from '../../images/pokedex.png'
+import { NavLink } from 'react-router-dom'
+import './Header.css';
+// import CloseIcon from '@mui/icons-material/Close';
+// import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faX} from '@fortawesome/free-solid-svg-icons'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+import { Dropdown } from 'bootstrap';
+import Search from './Search';
 
 function Header() {
+    const [click, setClick] = useState(false);
+    const [dropwdon,setDropdown] = useState(false)
+
+    const handleClick = () => setClick(!click);
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" sticky="top"  >
-      <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <NavDropdown title="Filter" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+    <nav className="navbar">
+        <div className="navbar-container">
+            <NavLink to="/" className="navbar-logo">
+                <a href="" className='nav-links'>
+                    <img className='logo' src={pokedex} alt="" />
+                </a>
+            </NavLink>
+            <div className='menu-icon' onClick={handleClick}>
+                <FontAwesomeIcon icon={click ? faX : faBars}/>
+            </div>
+            <ul className={click ?  'nav-menu active': 'nav-menu'}>
+                <li className='nav-item'>
+                    <Search/>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    </>
   )
 }
 
